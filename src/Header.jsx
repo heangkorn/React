54b8c0cd0a540b72f0d3510bd58dc8a0779fca54
KORN Laiheang <laiheang5854465@gmail.com>
2024-07-15 23:03:18 +0700

    first commit

---
+++ src/Header.jsx
@@ -0,0 +1,17 @@
+function Header(){
+  return(
+    <header>
+      <h1>My Website</h1>
+      <nav>
+        <u1>
+          <li><a href="#">Home </a></li>
+          <li><a href="#">About</a></li>
+          <li><a href="#">Service</a></li>
+          <li><a href="#">Contact</a></li>
+        </u1>
+      </nav>
+      <hr></hr>
+    </header>
+  )
+}
+export default Header
