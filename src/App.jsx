2843aac9cbe47025bca80efec427e056f9cfa94d
KORN Laiheang <laiheang5854465@gmail.com>
2024-07-16 18:32:05 +0700

    Inline

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
+// how to stlye react components with css
+//(not including external framwork or preprocessors)
+//1. external
+//2. modules
+//3. Inline 
+import Button from './Button.jsx'
 
-
-import Card from './Card.jsx'
 function App() {
   return(
-    <>
+  
+   <Button/>
    
-    <Card/>
-    <Card/>
-    <Card/>
-   
-   
-    </>
+    
   );
 }
 
---
+++ src/Button.jsx
@@ -0,0 +1,12 @@
+
+function Button (){
+  const styles = {
+    
+      backgroundColor: "aqua",
+      color: "brown",
+      borderradius: "5px",
+    
+  } 
+  return (<button style={stlyes}>Click me</button>  );
+}
+export default Button 
