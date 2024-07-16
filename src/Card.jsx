82da4a2c1534ee84c9b002ca62ffbe5903d9e80c
KORN Laiheang <laiheang5854465@gmail.com>
2024-07-16 12:59:21 +0700

    Card Components

--- src/Card.jsx
+++ src/Card.jsx
@@ -1,5 +1,12 @@
-
-function Card (){
+import profilePic from './assets/saerosoft_white.jpg'
+function Card(){
+ return(
+  <div className="card">
+     <img className="card-image"src={profilePic} alt="profile picture"></img>
+     <h2 className="card-title">Bro Code</h2>
+     <p className='card-text'>I am a student and play game every day</p>
+   </div>
+ )
 
 }
 export default Card
